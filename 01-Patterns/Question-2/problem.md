7323d896f4b80cf25477bf034b5c8cff98905336
GUPTA Saksham (CNH Industrial) <c11532b@cnh1.cnhgroup.cnh.com>
2023-08-16 15:17:09 +0530

    Patterns - Question2

---
+++ 01-Patterns/Question-2/problem.md
@@ -0,0 +1,14 @@
+### Input: 5
+
+### Output
+
+```text
+* 
+* * 
+* * * 
+* * * * 
+* * * * *
+```
+
+
+[Question Link](https://practice.geeksforgeeks.org/problems/right-triangle/1)
--- 01-Patterns/Question-2/solution.js
+++ 01-Patterns/Question-2/solution.js
@@ -1,7 +1,7 @@
 function printPattern(n) {
   let output = "";
   for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
+    for (let j = 0; j <= i; j++) {
       output += "* ";
     }
     output += "\n";
